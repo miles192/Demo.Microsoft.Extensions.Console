751900fd11dcd426dda0c4d39b6c016a11f06627
miles.davies <miles.davies@horiba.com>
2020-07-10 15:44:46 +0100

    switched to arguments in the configuration

--- Demo.Microsoft.Extensions.Console/Program.cs
+++ Demo.Microsoft.Extensions.Console/Program.cs
@@ -1,28 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace Demo.Microsoft.Extensions.CLI
 {
     internal class Program
     {
-        private static async Task<int> Main(string[] args)
+        private static async Task<int> Main()
         {
-            return await Task.Run(() => DefaultVerb(args));
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("configuration.json")
+                .Build();
 
+            return await Task.Run(() => DefaultVerb(configuration));
         }
 
         /// <summary> Default verb. </summary>
         /// <remarks> This is a little action to use as a stub. </remarks>
-        /// <param name="args"> The arguments. </param>
+        /// <param name="configuration"></param>
         /// <returns> An int. </returns>
-        private static int DefaultVerb(IEnumerable<string> args)
+        private static int DefaultVerb(IConfiguration configuration)
         {
             var currentForeground = Console.ForegroundColor;
             try
             {
-                Console.WriteLine(@"Default: Hello world!");
+                Console.WriteLine(configuration["Demo:Banner"]);
                 Console.ForegroundColor = ConsoleColor.Gray;
+
+                // this is how to process a json array of literals from the config file
+                var args = configuration.GetSection("Demo:Arguments")
+                    .GetChildren()
+                    .Select(arg => arg.Value)
+                    .ToArray();
+
                 Console.WriteLine($"Verb: Default called with {string.Join(",", args)}...");
                 return 0;
             }
